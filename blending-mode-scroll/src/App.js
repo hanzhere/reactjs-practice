66831f62bf025f236d2fdcac588ceed1713ee82a
hahaha <nguyensonha.hanz@gmail.com>
2020-10-17 21:53:42 +0700

    blending mode on scroll

---
+++ blending-mode-scroll/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <span className="text">Dont expect too much, enjoy what you have</span>
+      </div>
+      <div className="white"></div>
+      <div className="black"></div>
+      <div className="white"></div>
+      <div className="black"></div>
+      <div className="white"></div>
+      <div className="black"></div>
+      <div className="white"></div>
+      <div className="black"></div>
+    </div>
+  );
+}
+
+export default App;
